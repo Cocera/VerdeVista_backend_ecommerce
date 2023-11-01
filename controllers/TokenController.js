106887d596c7393da8341ef6f0782ff351cc5be2
Cocera <alcocera@gmail.com>
2023-11-02 00:51:13 +0100

    find user tokens and break permission token

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -5,6 +5,22 @@ const TokenController = {
         Token.findAll()
             .then(user => res.status(200).send({user}))
             .catch(err => console.error(err));
+    },
+    findUser(req, res) {
+        Token.findOne({
+            where: {
+                UserId: req.params.id
+            }})
+            .then(user => res.status(200).send({user}))
+            .catch(err => console.error(err));
+    },
+    breakPermission(req, res) {
+        // Si el id no coincide con nuestra db de users, devolver error concreto
+        Token.update(
+            { permission: false },
+            { where: {UserId: req.params.id}})
+            .then(user => res.status(200).send({message:'Permission updated successfully', user})) // user devuelve solo '1'
+            .catch(err => console.error(err));
     }
 };
 
--- routes/tokens.js
+++ routes/tokens.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const TokenController = require('../controllers/TokenController.js');
 
-router.get("/", TokenController.findAll);
+router.get('/', TokenController.findAll);
+router.get('/id/:id', TokenController.findUser);
+router.get('/break/id/:id', TokenController.breakPermission);
 
 module.exports = router;
