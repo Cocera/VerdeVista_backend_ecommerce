429bc4a400a9146e26ccdf9852ac6f208834485e
Cocera <alcocera@gmail.com>
2023-11-02 23:25:03 +0100

    order destroy + relation in OrderProduct

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,4 +1,4 @@
-const { Order, Product } = require('../models/index.js');
+const { Order, Product, OrderProduct } = require('../models/index.js');
 
 const OrderController = {
     create(req, res) {
@@ -17,6 +17,23 @@ const OrderController = {
             include: [{model:Product, attributes: ['name','price'], through: { attributes:[]}}],
         })
         .then(order => res.status(200).send(order))
+        .catch(err => {
+            console.error(err);
+            res.status(500).send(err);
+        });
+    },
+    delete(req, res) {
+        Order.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        OrderProduct.destroy({
+            where: {
+                ProductId: req.params.id
+            }
+        })
+        .then(res.status(200).send({ message: 'The order has been removed'}))
         .catch(err => {
             console.error(err);
             res.status(500).send(err);
--- routes/orders.js
+++ routes/orders.js
@@ -4,5 +4,6 @@ const OrderController = require('../controllers/OrderController.js');
 
 router.post('/', OrderController.create);
 router.get('/', OrderController.findAll);
+router.delete('/id/:id', OrderController.delete);
 
 module.exports = router;
