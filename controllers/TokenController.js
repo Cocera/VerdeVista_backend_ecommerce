c432a4d01071698288acf087e9bb52c50d5dc0e5
Cocera <alcocera@gmail.com>
2023-11-10 09:28:34 +0100

    deleted code commented

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -38,29 +38,4 @@ const TokenController = {
     }
 };
 
-
-// const TokenController = {
-//     findAll(req, res) {
-//         Token.findAll()
-//             .then(user => res.status(200).send({user}))
-//             .catch(err => console.error(err));
-//     },
-//     findUser(req, res) {
-//         Token.findAll({
-//             where: {
-//                 UserId: req.params.id
-//             }})
-//             .then(user => res.status(200).send({user}))
-//             .catch(err => console.error(err));
-//     },
-//     breakPermission(req, res) {
-//         // Si el id no coincide con nuestra db de users, devolver error concreto
-//         Token.update(
-//             { permission: false },
-//             { where: {UserId: req.params.id}})
-//             .then(user => res.status(200).send({message:'Permission updated successfully', user})) // user devuelve solo '1'
-//             .catch(err => console.error(err));
-//     }
-// };
-
 module.exports = TokenController;
